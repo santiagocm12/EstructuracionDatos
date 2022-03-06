7c0cfd876891ff7f769015a3c540f48f702519b3
unknown <ssanabriag@unbosque.edu.co>
2022-03-06 14:58:44 -0500

    creacion de modelo o clase dados, veridica si los numero son iguales y la suma de los dos :3

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -1,10 +1,13 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.Dados;
+
 public class AplMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Controller c = new Controller();
+		//Dados d = new Dados();
 	}
 
 }
---
+++ src/co/edu/unbosque/model/Dados.java
@@ -0,0 +1,66 @@
+package co.edu.unbosque.model;
+
+public class Dados {
+	private int number1;
+	private int number2;
+	private int total;
+	private boolean par;
+	
+	public Dados() {
+		this.number1 = (int) Math.floor(Math.random()*6+1);;
+		this.number2 = (int) Math.floor(Math.random()*6+1);;
+		this.total = number1+number2;
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println("Total de los dados es: "+total);
+		System.out.println(NumPar());
+		NumPar();
+	}
+	
+	public boolean NumPar() {
+		if (number1==number2) {
+			par = true;
+			return par;
+		}
+		else {
+			par = false;
+		return par;
+		
+	  }
+   }
+
+	public int getNumber1() {
+		return number1;
+	}
+
+	public void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+
+	public int getNumber2() {
+		return number2;
+	}
+
+	public void setNumber2(int number2) {
+		this.number2 = number2;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public boolean isPar() {
+		return par;
+	}
+
+	public void setPar(boolean par) {
+		this.par = par;
+	}
+	
+	
+	
+}
